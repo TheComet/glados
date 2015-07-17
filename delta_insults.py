55cf611b95f8a0b174babee079dc4e57da5a6587
Neil Moore <dar13.moore@gmail.com>
2015-07-17 18:31:04 -0400

    Fixed regexes and added a new one

--- delta_insults.py
+++ delta_insults.py
@@ -7,13 +7,17 @@ chill_out = ["Woahhh, chill the fuck out bro", "Says the guy with two gay daddie
 def what_i_thought(bot, trigger):
 	bot.say(trigger.nick + ": Oh yeah? Think again")
 
-@module.rule("^.*(according).*(book)*.$")
+@module.rule("^(?=.*according)(?=.*book).*$")
 def according_to_books(bot, trigger):
 	bot.say(trigger.nick + ": Just because you read lots of books doesn't mean mommy loves you")
 
-@module.rule("^.*(fuck).*(queer)*.$")
+@module.rule("^((?=.*fucking)(?=.*queer).*|(?=.*FUCKING).*)$")
 def fucking_queer_defense(bot, trigger):
 	global chill_out
 	global chill_out_counter
 	bot.say(trigger.nick + ": " + chill_out[chill_out_counter])
 	chill_out_counter = (chill_out_counter + 1) % len(chill_out)
+
+@module.rule("^(((?=.*goddammit).*)|((?=.*goddamnit).*)|((?=.*goddangit).*)).*$")
+def allah_dammit(bot, trigger):
+        bot.say(trigger.nick + ": Allah dammit!")
