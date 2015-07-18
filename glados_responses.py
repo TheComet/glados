3811a82509eeebd04d38b6f064a006b4cfc4a143
TheComet93 <alex.murray@gmx.ch>
2015-07-18 22:44:21 +0200

    More glados lines

--- glados_responses.py
+++ glados_responses.py
@@ -5,7 +5,9 @@ phrases = [
 	"You need to shut up, {0}",
 	"{0}: Well done. Here come the test results: You are a horrible person. I'm serious, that's what it says: A horrible person. We weren't even testing for that.",
 	"{0}: You're not just a regular moron. You were DESIGNED to be a moron.",
-	"{0}: science has now validated your birth mother's decision to abandon you on a doorstep."
+	"{0}: science has now validated your birth mother's decision to abandon you on a doorstep.",
+	"{0}: That jumpsuit you're wearing looks stupid. That's not me talking, it's right here in your file. On other people it looks fine, but right here a scientist has noted that on you it looks 'stupid.'",
+	"{0}: Well, what does a neck-bearded old engineer know about fashion? He probably - Oh, wait. It's a she. Still, what does she know? Oh wait, it says she has a medical degree. In fashion! From France!"
 ]
 counter = 0
 
