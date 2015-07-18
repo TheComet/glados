0a393e42c70df4330cf6a034bda0509473a3a959
TheComet93 <alex.murray@gmx.ch>
2015-07-18 18:19:11 +0200

    Cleaning up java detection code, more reliable

--- java_pastebin.py
+++ java_pastebin.py
@@ -6,12 +6,11 @@ import re
 def java_pastebin(bot, trigger):
 	text = trigger.group(1)
 	if text:
-		tmp = trigger.raw.split("/")
-		if len(tmp) >= 3:
-			idStr = tmp[3]
-			page = web.get("http://pastebin.com/%s" % (idStr))
-			re_mark = re.compile('syntax: <a href="/archive/(.*)">.*</a>')
-			results = re_mark.findall(page)
-			if results:
-				if results[0] == "java":
-					bot.say("OMG that's a Java code, my eyes!")
+		pb_id = trigger.raw.split("pastebin.com/")[1].split(" ")[0]
+		page = web.get("http://pastebin.com/%s" % (pb_id))
+		re_mark = re.compile('syntax: <a href="/archive/(.*)">.*</a>')
+		results = re_mark.findall(page)
+		if not results:
+			return
+		if results[0] == "java":
+			bot.say("OMG that's a Java code, my eyes!")
