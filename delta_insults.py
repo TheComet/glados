49aa1d532f76b19bb2aafe2164b9a873cd586120
TheComet93 <alex.murray@gmx.ch>
2015-07-18 04:06:46 +0200

    Initial commit

---
+++ delta_insults.py
@@ -0,0 +1,19 @@
+from willie import module
+
+chill_out_counter = 0
+chill_out = ["Woahhh, chill the fuck out bro", "Says the guy with two gay daddies", "That was uncalled for"]
+
+@module.rule("^.*what i thought*.$")
+def what_i_thought(bot, trigger):
+	bot.say(trigger.nick + ": Oh yeah? Think again")
+
+@module.rule("^.*(according).*(book)*.$")
+def according_to_books(bot, trigger):
+	bot.say(trigger.nick + ": Just because you read lots of books doesn't mean mommy loves you")
+
+@module.rule("^.*(fuck).*(queer)*.$")
+def fucking_queer_defense(bot, trigger):
+	global chill_out
+	global chill_out_counter
+	bot.say(trigger.nick + ": " + chill_out[chill_out_counter])
+	chill_out_counter = (chill_out_counter + 1) % len(chill_out)
