4b4ccf8ac59e54f62cad7bc72dd6c986496714c6
TheComet93 <alex.murray@gmx.ch>
2015-07-18 22:20:26 +0200

    Adding two more phrases to glados responses

--- glados_responses.py
+++ glados_responses.py
@@ -1,13 +1,18 @@
 from willie import module
 
+# NOTE: You *HAVE* to use one {0} in each string - this gets replaced by the nick
 phrases = [
-	"You need to shut up, {0}"
+	"You need to shut up, {0}",
+	"{0}: Well done. Here come the test results: You are a horrible person. I'm serious, that's what it says: A horrible person. We weren't even testing for that.",
+	"{0}: You're not just a regular moron. You were DESIGNED to be a moron."
 ]
 counter = 0
 
 @module.rule("^.*(shut)(up)(glados).*$")
+@module.rule("^.*(fuck)(you)(glados).*$")
 def shut_up(bot, trigger):
 	global phrases
 	global counter
 	bot.say(phrases[counter].format(trigger.nick))
 	counter = (counter + 1) % len(phrases)
+
