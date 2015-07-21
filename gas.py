d31157d38b0841690d4aaf1825e8ba0f7fc6b496
TheComet93 <alex.murray@gmx.ch>
2015-07-21 22:14:51 +0200

    Forgot a colon

--- gas.py
+++ gas.py
@@ -5,7 +5,7 @@ def gas(bot, trigger):
     if not trigger.group(2):
         bot.say("Ab ins gas du Jude!")
     else:
-        if not trigger.group(2).lower().find("glados") == -1
+        if not trigger.group(2).lower().find("glados") == -1:
             bot.say("Nice try. Get comfortable while I warm up the neurotoxin emitters.")
         else:
             bot.say(trigger.group(2) + ": Ab ins gas du Jude!")
