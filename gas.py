3808514dfa96c77466eabd3ab293530829d34233
TheComet93 <alex.murray@gmx.ch>
2015-07-21 22:07:20 +0200

    Don't gas GLaDOS

--- gas.py
+++ gas.py
@@ -5,5 +5,8 @@ def gas(bot, trigger):
     if not trigger.group(2):
         bot.say("Ab ins gas du Jude!")
     else:
-        bot.say(trigger.group(2) + ": Ab ins gas du Jude!")
+        if trigger.group(2) == "GLaDOS":
+            bot.say("Nice try. Get comfortable while I warm up the neurotoxin emitters.")
+        else:
+            bot.say(trigger.group(2) + ": Ab ins gas du Jude!")
 
