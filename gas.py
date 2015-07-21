83c5e6d7cacce8d401e99c1f648e0af0db3c1e66
TheComet93 <alex.murray@gmx.ch>
2015-07-21 22:13:31 +0200

    lsdkj

--- gas.py
+++ gas.py
@@ -5,7 +5,7 @@ def gas(bot, trigger):
     if not trigger.group(2):
         bot.say("Ab ins gas du Jude!")
     else:
-        if trigger.group(2).lower()  == "glados":
+        if not trigger.group(2).lower().find("glados") == -1
             bot.say("Nice try. Get comfortable while I warm up the neurotoxin emitters.")
         else:
             bot.say(trigger.group(2) + ": Ab ins gas du Jude!")
