41bc6e0a525b3cfdb2ae84560e29c4ff33c39ab7
TheComet93 <alex.murray@gmx.ch>
2015-07-22 02:06:22 +0200

    Adding bastard operator from hell excuses

--- fortune.py
+++ fortune.py
@@ -7,3 +7,9 @@ def fortune(bot, trigger):
 	fortune = commands.getoutput('/usr/games/fortune')
 	for line in fortune.rsplit("\n"):
 		bot.say(line)
+
+@module.commands('bofh')
+def bastard_operator_from_hell_quote(bot, trigger):
+    excuse = commands.getoutput("/usr/games/fortune bofh-excuses | tr '\n' ' '")
+    for line in excuse.rsplit("\n"):
+        bot.say(line)
